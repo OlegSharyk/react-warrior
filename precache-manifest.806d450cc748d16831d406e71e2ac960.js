a44c712854460d8f61fd9243c37febcd9ebc4996
olegs <olegs@techfinancials.com>
2020-04-02 13:39:46 +0300

    Updates

---
+++ precache-manifest.806d450cc748d16831d406e71e2ac960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2bfe50baa0ab1d73a94356c1a16eaf",
+    "url": "/react-warrior/index.html"
+  },
+  {
+    "revision": "9aa6e4bb4b8aa44e2293",
+    "url": "/react-warrior/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4d1f55cca7c3f18d8666",
+    "url": "/react-warrior/static/css/main.d17f0a4c.chunk.css"
+  },
+  {
+    "revision": "9aa6e4bb4b8aa44e2293",
+    "url": "/react-warrior/static/js/2.33911f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-warrior/static/js/2.33911f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1f55cca7c3f18d8666",
+    "url": "/react-warrior/static/js/main.c94f4ffa.chunk.js"
+  },
+  {
+    "revision": "42e58f909d4814e7975f",
+    "url": "/react-warrior/static/js/runtime-main.60903819.js"
+  }
+]);
